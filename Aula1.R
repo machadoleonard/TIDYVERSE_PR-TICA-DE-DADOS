a1a47476a4240c844443b8f2533132ba6dc4b9d7
machadoleonard <machadolrp@uol.com.br>
2022-06-25 09:44:11 -0300

    SELECT()
    Usando a função select ()

--- Aula1.R
+++ Aula1.R
@@ -1,3 +1,26 @@
-getwd()
-setwd("C:/Users/Leonardo Machado/Documents/CURSOS R/Rstudio com GitHub/TIDYVERSE_PR-TICA-DE-DADOS")
-getwd()
+# Tidyverse
+
+# Select
+
+library(tidyverse)
+
+data("starwars")
+?starwars
+
+starwars %>% 
+  select(name)
+
+
+starwars %>% 
+  select(name,mass)
+
+starwars %>% 
+  select(name : skin_color)
+starwars %>% 
+  select(-name)
+starwars %>% 
+  select(starts_with("s"))
+starwars %>% 
+  select(-films ,    -vehicles ,- starships)
+starwars %>% 
+  select(ends_with("color"))
